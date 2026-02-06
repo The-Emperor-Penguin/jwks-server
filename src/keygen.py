a9f1755ac0198c25fa238889e8f45bc71335c63c
The-Emperor-Penguin <emperor@thepenguin.me>
2026-02-06 16:23:19 -0600

    feat: add authentication and JWKS endpoints to server
    - Implemented /auth endpoint for JWT issuance, supporting expired keys.
    - Created /.well-known/jwks.json endpoint to serve valid public keys in JWKS format.
    - Initialized key manager with one valid and one expired key for testing.

--- src/keygen.py
+++ src/keygen.py
@@ -3,32 +3,26 @@ import uuid
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 
-def create_key_pair(experation: datetime.datetime):
+def create_key_pair():
     """The Experation date must be in UTC or will be converted to UTC
     """
 
     EXPONENT = 65537
-
-    experation = experation.astimezone(datetime.UTC)
-
-
-    if (experation < datetime.datetime.now(datetime.UTC)):
-        print("Warning experation date is in the past!")
-        raise OSError
 
     private_key = rsa.generate_private_key(
         public_exponent=EXPONENT,
         key_size=2048,
     )
 
-    public_key = private_key.public_key
+    public_key = private_key.public_key()
 
     kid = uuid.uuid4()
 
-    jkw = {"kty": "rsa", "kid": kid, "experation": str(experation), "e": EXPONENT}
+    jkw = {"kty": "RSA", "kid": str(kid),"alg": "RS256", "use": "sig", "e": EXPONENT}
 
-    return jkw
+    return (private_key, public_key, jkw)
 
 
 
-create_key_pair(datetime.datetime(2027,5,5,5,5,5))
+if __name__ == "__main__":
+    create_key_pair()
--- src/keyman.py
+++ src/keyman.py
@@ -1,13 +1,46 @@
 import keygen
-from datetime import datetime
+from datetime import datetime, UTC
 
 class KeyManager:
     keys = []
     def __init__(self):
         pass
-    def create_key(self, experation: datetime):
-        pass
-    def obtain_key(kid):
-        pass
-    def obtain_exp_key(kid):
-        pass
+    def create_key(self, experation: datetime, debug=False):
+
+        experation = experation.astimezone(UTC)
+        print(experation)
+        if (experation < datetime.now(UTC)) and not debug:
+            print("Warning experation date is in the past!")
+            raise IOError
+
+        private_key, public_key, jwk = keygen.create_key_pair()
+        self.keys.append({"private_key": private_key, "public_key": public_key, "jwk": jwk, "exp": experation})
+
+
+    def obtain_key(self, kid: str):
+        '''
+        May not be used to obtain an expired key.
+        Inorder to obtain an expired key you must call obtain_exp_key function with safe = False.
+        
+        :param kid: the key ID for the key that is requested
+        '''
+        return self.obtain_exp_key(kid)
+
+    def obtain_exp_key(self, kid, safe = True):
+        '''
+        May be used to obtain an expired key.
+        To obtain an expired key you must have safe = False.
+        
+        :param kid: the key ID for the key that is requested
+        :param safe: Allow expired keys to be returned?
+        '''
+            
+        
+        for keypair in self.keys:
+            if keypair['jwk']['kid'] == kid:
+                if (keypair['exp'] <= datetime.now(tz=datetime.UTC)) and safe:
+                    print("Requested expired key")
+                    return {}
+                return keypair
+
+        return {}
