a3d235382579b72e6cf81593fb50ab8228ba2d54
The-Emperor-Penguin <emperor@thepenguin.me>
2026-02-06 12:57:35 -0600

    prep work for key manager

--- src/keygen.py
+++ src/keygen.py
@@ -1,21 +1,23 @@
 import datetime
+import uuid
+from cryptography.hazmat.primitives.asymmetric import rsa
+
 
 def create_key_pair(experation: datetime.datetime):
-    '''
-    The Experation date must be in UTC or will be converted to UTC
-    '''
-    from cryptography.hazmat.primitives.asymmetric import rsa
-    import uuid
+    """The Experation date must be in UTC or will be converted to UTC
+    """
 
-    experation = experation.astimezone(datetime.timezone.utc)
+    EXPONENT = 65537
+
+    experation = experation.astimezone(datetime.UTC)
 
 
-    if (experation < datetime.datetime.now(datetime.timezone.utc)):
-            print("Warning experation date is in the past!")
-            raise IOError
+    if (experation < datetime.datetime.now(datetime.UTC)):
+        print("Warning experation date is in the past!")
+        raise OSError
 
     private_key = rsa.generate_private_key(
-        public_exponent=65537,
+        public_exponent=EXPONENT,
         key_size=2048,
     )
 
@@ -23,10 +25,10 @@ def create_key_pair(experation: datetime.datetime):
 
     kid = uuid.uuid4()
 
-    jkw = {"public_key": public_key, "kid": kid, "experation": experation}
-    print(jkw)
+    jkw = {"kty": "rsa", "kid": kid, "experation": str(experation), "e": EXPONENT}
+
+    return jkw
 
 
-    
 
 create_key_pair(datetime.datetime(2027,5,5,5,5,5))
---
+++ src/keyman.py
@@ -0,0 +1,13 @@
+import keygen
+from datetime import datetime
+
+class KeyManager:
+    keys = []
+    def __init__(self):
+        pass
+    def create_key(self, experation: datetime):
+        pass
+    def obtain_key(kid):
+        pass
+    def obtain_exp_key(kid):
+        pass
