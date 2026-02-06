ff8e52f7c43f471d509499928357d81f96dca6e7
The-Emperor-Penguin <emperor@thepenguin.me>
2026-02-05 23:54:07 -0600

    implement create_key_pair function for key generation with expiration validation still need to finish function as i still do not understand the format for jkw

--- src/keygen.py
+++ src/keygen.py
@@ -1,2 +1,32 @@
-import cryptography
+import datetime
 
+def create_key_pair(experation: datetime.datetime):
+    '''
+    The Experation date must be in UTC or will be converted to UTC
+    '''
+    from cryptography.hazmat.primitives.asymmetric import rsa
+    import uuid
+
+    experation = experation.astimezone(datetime.timezone.utc)
+
+
+    if (experation < datetime.datetime.now(datetime.timezone.utc)):
+            print("Warning experation date is in the past!")
+            raise IOError
+
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+
+    public_key = private_key.public_key
+
+    kid = uuid.uuid4()
+
+    jkw = {"public_key": public_key, "kid": kid, "experation": experation}
+    print(jkw)
+
+
+    
+
+create_key_pair(datetime.datetime(2027,5,5,5,5,5))
